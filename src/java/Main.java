1e207c60cfb6fcacd1d8dd3ff4c26cfe8ee56ec5
Alex <abdullayevalibek4624@gmail.com>
2026-06-04 18:22:46 +0500

    The logic is created

--- src/java/Main.java
+++ src/java/Main.java
@@ -1,5 +1,23 @@
+import java.text.NumberFormat;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
-        System.out.println("Start of the work ladies and gentlemen!");
+        final byte MONTHS = 12;
+        final byte PERCENT = 100;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Principal:");
+        int principal = input.nextInt();
+        System.out.print("Annual Interest Rate:");
+        float annualInterestRate = input.nextFloat();
+        System.out.print("Period(in years):");
+        byte period = input.nextByte();
+        float monthlyInterestRate = annualInterestRate/PERCENT/MONTHS;
+        short numberOfPayments = (short)(period*MONTHS);
+        double mortgage = principal*(monthlyInterestRate*Math.pow((1+monthlyInterestRate),numberOfPayments))/(Math.pow((1+monthlyInterestRate),numberOfPayments)-1);
+        System.out.println("Principal: "+NumberFormat.getCurrencyInstance().format(principal));
+        System.out.println("Annual Interest Rate: "+NumberFormat.getPercentInstance().format(annualInterestRate/PERCENT));
+        System.out.println("Period: "+ period);
+        System.out.println("Mortgage: "+ NumberFormat.getCurrencyInstance().format(mortgage));
     }
 }
