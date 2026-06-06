80ffe238da7c7c6806b5d3b70099a91f9f60d829
Alex <abdullayevalibek4624@gmail.com>
2026-06-06 23:53:18 +0500

    Add basic error handling

--- src/java/Main.java
+++ src/java/Main.java
@@ -6,12 +6,33 @@ public class Main {
         final byte PERCENT = 100;
 
         Scanner input = new Scanner(System.in);
-        System.out.print("Principal:");
-        int principal = input.nextInt();
+        System.out.print("Principal ($1K - $1M):");
+        int principal;
+        while (true) {
+            principal = input.nextInt();
+            if (principal >= 1_000 && principal <= 1_000_000)
+                break;
+            else
+                System.out.println("Enter a number from 1,000 to 1,000,000 please...");
+        }
         System.out.print("Annual Interest Rate:");
-        float annualInterestRate = input.nextFloat();
+        float annualInterestRate;
+        while (true) {
+            annualInterestRate = input.nextFloat();
+            if (annualInterestRate > 0 && annualInterestRate <= 30)
+                break;
+            else
+                System.out.println("Enter a value from 1 to 30 please...");
+        }
         System.out.print("Period(in years):");
-        byte period = input.nextByte();
+        byte period;
+        while (true) {
+            period = input.nextByte();
+            if (period > 0 && period <=30)
+                break;
+            else
+                System.out.println("Enter a value from 1 to 30 please<3");
+        }
         float monthlyInterestRate = annualInterestRate/PERCENT/MONTHS;
         short numberOfPayments = (short)(period*MONTHS);
         double mortgage = principal*(monthlyInterestRate*Math.pow((1+monthlyInterestRate),numberOfPayments))/(Math.pow((1+monthlyInterestRate),numberOfPayments)-1);
